23c7df469162c4525fd8987b27a699d3c4ced814
heckenmann <benedikt.stoffel@t-online.de>
2016-03-05 11:44:38 +0100

    init

---
+++ src/main/java/de/heckenmann/jenkinsdocker/HelloWorld.java
@@ -0,0 +1,12 @@
+package de.heckenmann.jenkinsdocker;
+
+/**
+ *
+ * @author heckenmann
+ */
+public class HelloWorld {
+
+    public static void main(String... args) {
+        System.out.println("Hallo " + System.getProperty("name"));
+    }
+}
